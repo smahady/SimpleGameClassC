a21ae8f32ef477b1a621edd636dc7de8411e6c1d
Sean Mahady <52180359+smahady@users.noreply.github.com>
2021-07-04 01:59:22 +0000

    fix

--- main.py
+++ main.py
@@ -252,24 +252,24 @@ class Kamille(Character):
 	# Add a method called walkBehavior. 
 	# This should check if self.scene.keysDown[K_RIGHT]is True. If so self.facing to Facing.RIGHT, self.setCurrentCycle to Facing.RIGHT, call the self.startAnimation method. Set the DX to a value between 0 and 10
 	# If not check if self.scene.keysDown[K_LEFT] is True. If so self.facing to Facing.RIGHT, self.setCurrentCycle to Facing.RIGHT, call the self.startAnimation method. Set the DX to a value between 0 and -10
-def walkBehavior(self):
-  if self.scene.keysDown[Scene.K_RIGHT]:
-    self.facing = 0
-    self.setCurrentCycle(0)
-    self.playanimation()
-    self.dx = 5
-    self.state = States.WALK
-  elif self.scene.keysDown[Scene.K_LEFT]:
-    self.facing = 1
-    self.setCurrentCycle(1)
-    self.playanimation()
-    self.dx = -5
-    self.state = States.WALK
+  def walkBehavior(self):
+    if self.scene.keysDown[Scene.K_RIGHT]:
+      self.facing = 0
+      self.setCurrentCycle(0)
+      self.playanimation()
+      self.dx = 5
+      self.state = States.WALK
+    elif self.scene.keysDown[Scene.K_LEFT]:
+      self.facing = 1
+      self.setCurrentCycle(1)
+      self.playanimation()
+      self.dx = -5
+      self.state = States.WALK
 	# Add a method called jumpBehavior. This should set the dy to a negative number (moving up), and set the stateTimer to the number of frames before falling.
-def jumpBehavior(self):
-  self.stateTimer = 21
-  self.dy = -7
-  self.state=States.JUMP
+  def jumpBehavior(self):
+    self.stateTimer = 21
+    self.dy = -5
+   self.state=States.JUMP
 
 
 
